7d9715f9428bda31dea257456c3bf280ec5d903d
RAHULREEKHADI1 <158846588+RAHULREEKHADI1@users.noreply.github.com>
2025-04-16 00:23:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(cors({
     origin:[
         process.env.FRONTEND_URL,
-        "https://todo-app-backend-hfzb.onrender.com"
+        "https://todo-app-frontend-mfhd.onrender.com"
         ],
     credentials:true,
     methods:"GET,POST,PUT,DELETE",
