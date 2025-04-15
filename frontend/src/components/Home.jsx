f261d6df3743005776e74354a2b674aaf60875f8
RAHULREEKHADI1 <158846588+RAHULREEKHADI1@users.noreply.github.com>
2025-04-16 00:16:53 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
     const fetchTodos = async()=>{
       try{
         setLoading(true)
-        const response = await axios.get("http://localhost:4001/todo/fetch",{
+        const response = await axios.get("https://todo-app-backend-hfzb.onrender.com/todo/fetch",{
           withCredentials:true,
           headers:{
             "Content-Type":"application/json",  
@@ -34,7 +34,7 @@ function Home() {
   const todoCreate=async ()=>{
     if(!newTodo)return;
     try{
-      const response  = await axios.post("http://localhost:4001/todo/create",{
+      const response  = await axios.post("https://todo-app-backend-hfzb.onrender.com/todo/create",{
         title:newTodo,
         completed:false,
       },{
@@ -52,7 +52,7 @@ function Home() {
     const todo = todos.find((t) => t._id === id);
     try {
       const response = await axios.put(
-        `http://localhost:4001/todo/update/${id}`,
+        `https://todo-app-backend-hfzb.onrender.com/todo/update/${id}`,
         {
           ...todo,
           completed: !todo.completed,
@@ -70,7 +70,7 @@ function Home() {
 
   const todoDelete = async(id)=>{
     try{
-      await axios.delete(`http://localhost:4001/delete/${id}`,{
+      await axios.delete(`https://todo-app-backend-hfzb.onrender.com/delete/${id}`,{
         withCredentials:true
       })
       setTodos(todos.filter((t)=>t._id!==id))
@@ -81,7 +81,7 @@ function Home() {
   const navigateTo = useNavigate();
   const logout = async()=>{
     try{
-      await axios.get("http://localhost:4001/user/logout",{
+      await axios.get("https://todo-app-backend-hfzb.onrender.com/user/logout",{
         withCredentials:true,
       })
       toast.success("User logged out successfully")
